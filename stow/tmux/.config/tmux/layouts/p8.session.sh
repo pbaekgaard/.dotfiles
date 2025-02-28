389f8332b0eaa182918c3c84603df24f92005988
pbaekgaard <peter.baekgaard@outlook.com>
2025-02-28 13:39:36 +0100

    updated neovim

--- stow/tmux/.config/tmux/layouts/p8.session.sh
+++ stow/tmux/.config/tmux/layouts/p8.session.sh
@@ -2,14 +2,14 @@
 # Must be called before `initialize_session`.
 TYPE="school"
 #TYPE="dev"
-session_folder_root="$HOME/$TYPE/{{SESSION_NAME}}"
+session_folder_root="$HOME/$TYPE/p8"
 mkdir -p $session_folder_root
 
-session_root "~/$TYPE/{{SESSION_NAME}}"
+session_root "~/$TYPE/p8"
 
 # Create session with specified name if it does not already exist. If no
 # argument is given, session name will be based on layout file name.
-if initialize_session "{{SESSION_NAME}}"; then
+if initialize_session "p8"; then
 
   # Create a new window inline within session layout definition.
   new_window "nvim"
@@ -19,7 +19,8 @@ if initialize_session "{{SESSION_NAME}}"; then
   #load_window "example"
 
   # Select the default active window on session creation.
-  #select_window 1
+  select_window 1
+  run_cmd "nvim ."
 
 fi
 
