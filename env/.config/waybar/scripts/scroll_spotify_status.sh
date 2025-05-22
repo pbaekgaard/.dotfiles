1e569ae7ad74d3cd9bd6a713506982dfd7d2afe8
pbaekgaard <pbakga21@student.aau.dk>
2025-05-22 13:39:39 +0200

    dotcommit

--- env/.config/waybar/scripts/scroll_spotify_status.sh
+++ env/.config/waybar/scripts/scroll_spotify_status.sh
@@ -4,7 +4,5 @@ zscroll -l 30 \
         --delay $delay \
         --scroll-padding "    " \
         --match-command "`dirname $0`/get_spotify_status.sh --status" \
-        --match-text "Playing" "--scroll 1" \
-        --match-text "Paused" "--scroll 0" \
         --always-reprint 1\
         --update-check true "`dirname $0`/get_spotify_status.sh"
