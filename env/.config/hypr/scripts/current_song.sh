63c78d8bf68d4d17ec55c93833f5edef8973cf71
pbaekgaard <pbakga21@student.aau.dk>
2025-05-22 12:47:43 +0200

    dotcommit

--- env/.config/hypr/scripts/current_song.sh
+++ env/.config/hypr/scripts/current_song.sh
@@ -1,5 +1,5 @@
 if playerctl -p spotify status &> /dev/null; then
-    echo "  $(playerctl -p spotify metadata title) - $(playerctl -p spotify metadata artist)"
+    printf " \u202F \u202F $(playerctl -p spotify metadata title) - $(playerctl -p spotify metadata artist)"
 else
     echo ""
 fi
