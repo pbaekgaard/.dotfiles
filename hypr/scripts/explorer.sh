bc1e076b221e1b48d57ddac98314dd43a5f9c900
pbaekgaard <pbakga21@student.aau.dk>
2025-05-26 14:13:36 +0200

    dotcommit

--- hypr/scripts/explorer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-THEME=catppuccin-mocha
-gsettings set org.gnome.desktop.interface gtk-theme "$THEME"
-GTK_THEME=$THEME nautilus
--- waybar/scripts/scroll_spotify_status.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-delay=0.2
-zscroll -l 30 \
-        --delay $delay \
-        --scroll-padding "    " \
-        --match-command "`dirname $0`/get_spotify_status.sh --status" \
-        --always-reprint 1\
-        --update-check true "`dirname $0`/get_spotify_status.sh"
